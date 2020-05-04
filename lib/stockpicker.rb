d848d81dbe8a0e85fed61e5aebbf229a4fa073a9
Randolph Cisneros <cisnerosRandolph@gmail.com>
2020-05-04 12:10:51 -0400

    stock-picker code cleanup

--- lib/stockpicker.rb
+++ lib/stockpicker.rb
@@ -6,20 +6,13 @@ class StockPicker
     end
 
     def stock_picker input_array
-        #puts input_array
         input_array.each do |day1|
-            #puts input_array.index(day1)
             daysAfter = input_array[input_array.index(day1) + 1..-1]
             daysAfter.each do |day2|
-                #puts input_array.index(day2)
                 if @bestPriceDiff == nil
                     assign_values(day1, day2, input_array)
-
-                    puts @bestPriceDiff
                 elsif day2 - day1 > @bestPriceDiff
-                    #puts(input_array.index(day1).to_s + " " + input_array.index(day2).to_s)
                     assign_values(day1, day2, input_array)
-                    puts @bestPriceDiff
                 else
                 end
             end
