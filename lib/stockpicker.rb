23012d45ae75c2f18cfea422912cf05127d30d6c
Randolph Cisneros <cisnerosRandolph@gmail.com>
2020-05-04 11:35:16 -0400

    stock-picker complete with test

--- lib/stockpicker.rb
+++ lib/stockpicker.rb
@@ -6,15 +6,20 @@ class StockPicker
     end
 
     def stock_picker input_array
-
+        #puts input_array
         input_array.each do |day1|
-            daysAfter = input_array[1..-1]
+            #puts input_array.index(day1)
+            daysAfter = input_array[input_array.index(day1) + 1..-1]
             daysAfter.each do |day2|
+                #puts input_array.index(day2)
                 if @bestPriceDiff == nil
-                    #Need to get the days from the array before calling this e.g. the element number
-                    assign_values(day1, day2)
+                    assign_values(day1, day2, input_array)
+
+                    puts @bestPriceDiff
                 elsif day2 - day1 > @bestPriceDiff
-                    assign_values(day1, day2)
+                    #puts(input_array.index(day1).to_s + " " + input_array.index(day2).to_s)
+                    assign_values(day1, day2, input_array)
+                    puts @bestPriceDiff
                 else
                 end
             end
@@ -22,9 +27,9 @@ class StockPicker
         return @daysForPriceDiff
     end
 
-    def assign_values day1, day2
+    def assign_values day1, day2, input_array
         @bestPriceDiff = day2 - day1
-        @daysForPriceDiff = [day1, day2]
+        @daysForPriceDiff = [input_array.index(day1), input_array.index(day2)]
     end
 
 end
--- test/stockpicker_spec.rb
+++ test/stockpicker_spec.rb
@@ -9,12 +9,12 @@ describe StockPicker do
     end
 
     it 'picks the best stocks' do
-        expect(@s.stock_picker([1,2,3,4,5])).to eq [1,5]
-        expect(@s.stock_picker([1,2,3,4,5])).to eq [1,5]
+        expect(@s.stock_picker([1,2,3,4,5])).to eq [0,4]
+        expect(@s.stock_picker([17,3,6,9,15,8,6,1,10])).to eq [1,4]
     end
 
 
     it 'gets the lowest losses' do
-        expect(@s.stock_picker([1,2,3,4,5].reverse)).to eq [1,2]
+        expect(@s.stock_picker([1,2,3,4,5].reverse)).to eq [0,1]
     end
 end
